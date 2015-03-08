2b8d3e6d9c7e1d2ee509e0859f94a47520674c16
reorx <novoreorx@gmail.com>
2015-03-08 16:25:47 +0800

    add requirements, update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@ __version__ = '1.0'
 
 from setuptools import setup
 
+
+def get_requires():
+    with open('requirements.txt', 'r') as f:
+        requires = [i for i in map(lambda x: x.strip(), f.readlines()) if i]
+    return requires
+
+get_requires()
+
 setup(
     name='readability',
     version=__version__,
     author='reorx',
     description='html main body extractor',
     py_modules=['readability'],
-    install_requires=[
-        'BeautifulSoup4==4.1.3',
-    ]
+    install_requires=get_requires()
 )
