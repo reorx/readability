fa3d1b9b86a5fc1b10e7fad8900b077bebbe8acf
reorx <novoreorx@gmail.com>
2015-03-08 16:31:20 +0800

    improve setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,21 @@ def get_requires():
         requires = [i for i in map(lambda x: x.strip(), f.readlines()) if i]
     return requires
 
-get_requires()
+
+def get_long_description():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
 
 setup(
     name='readability',
     version=__version__,
+    license='License :: OSI Approved :: MIT License',
     author='reorx',
+    author_email='novoreorx@gmail.com',
+    url='https://github.com/reorx/readability',
     description='html main body extractor',
+    long_description=get_long_description(),
     py_modules=['readability'],
     install_requires=get_requires()
 )
